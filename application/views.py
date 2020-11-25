b20ac35a793de8a0228956d7a467531f11cb418e
dmitri9149 <dmitri.as.name@gmail.com>
2020-11-25 09:50:56 +0200

    revert the file, was changed by mistake

--- application/views.py
+++ application/views.py
@@ -5,12 +5,3 @@ from application import app
 def index():
     return render_template("index.html")
 
-    @app.route("/tasks/new/")
-def tasks_form():
-    return render_template("tasks/new.html")
-
-@app.route("/tasks/", methods=["POST"])
-def tasks_create():
-    print(request.form.get("name"))
-  
-    return "hello world!"
