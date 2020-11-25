6409986ce94dfe6f2e4b5b64491060c817802c96
dmitri9149 <dmitri.as.name@gmail.com>
2020-11-25 09:46:55 +0200

    two more roots are added

--- application/views.py
+++ application/views.py
@@ -4,3 +4,13 @@ from application import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+    @app.route("/tasks/new/")
+def tasks_form():
+    return render_template("tasks/new.html")
+
+@app.route("/tasks/", methods=["POST"])
+def tasks_create():
+    print(request.form.get("name"))
+  
+    return "hello world!"
